fa1c32e64a030d1c5687d360850c7be6948bd352
danmorrison <dan@danm.it>
2018-10-15 07:54:53 +1100

    Initial Commit

---
+++ The Best/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Best
+//
+//  Created by Dan Morrison on 15/10/18.
+//  Copyright © 2018 Daniel Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
