de588d2a28812f712f4787ffb7c41ad58ccabc43
danmorrison <dan@danm.it>
2018-10-15 20:52:01 +1100

    Populate the picker with album release years

--- The Best/ViewController.swift
+++ The Best/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    var library = MPMediaQuery()
+    var years: Set<Int> = []
+    let calendar = Calendar.current
+    
+    @IBOutlet weak var yearPicker: UIPickerView!
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return yearPickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        return NSAttributedString(string: String(yearPickerData[row]))
+    }
+    
+    var yearPickerData: [Int] = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        self.yearPicker.delegate = self
+        self.yearPicker.dataSource = self
+
+        library = MPMediaQuery.albums()
+        library.groupingType = MPMediaGrouping.album
+        
+        for item in library.items! {
+            if item.releaseDate != nil {
+                years.insert(calendar.component(.year, from: item.releaseDate!))
+            }
+        }
+        
+        self.yearPickerData = years.sorted(by: >)
+    }
 
 }
 
