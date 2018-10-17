b17ed8d1dcef052aceccf4edcb5fc38e5bb73162
danmorrison <dan@danm.it>
2018-10-17 13:13:04 +1100

    Populate the table view with the albums that aren't in any of the best playlists

--- The Best/ViewController.swift
+++ The Best/ViewController.swift
@@ -11,36 +11,42 @@ import MediaPlayer
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDelegate, UITableViewDataSource {
     var library = MPMediaQuery()
-    var years: Set<Int> = []
     let calendar = Calendar.current
-    var yearPickerData = [Int]()
     var myBestAlbums = Set<UInt64>()
+    var theOtherAlbumsByYear = [(key: Int, value: [String])]()
     
     @IBOutlet weak var yearPicker: UIPickerView!
-
+    @IBOutlet weak var albumTable: UITableView!
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return yearPickerData.count
+        return theOtherAlbumsByYear.count
     }
     
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-        return NSAttributedString(string: String(yearPickerData[row]))
+        return NSAttributedString(string: String(theOtherAlbumsByYear[row].key))
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        albumTable.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return theOtherAlbumsByYear[yearPicker.selectedRow(inComponent: 0)].value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        cell.textLabel?.text = theOtherAlbumsByYear[yearPicker.selectedRow(inComponent: 0)].value[indexPath.row]
+        return cell
     }
     
     func refreshLibrary() {
+        // Which albums are the best?
         library = MPMediaQuery.playlists()
-        
         for playlist in library.collections! {
             if (playlist.value(forProperty: MPMediaPlaylistPropertyName) as! String).range(of: "^The Best ", options: .regularExpression) != nil {
                 for song in playlist.items {
@@ -48,18 +54,23 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 }
             }
         }
-        
+
+        // Which albums aren't the best?
         library = MPMediaQuery.albums()
-        
+        var theOtherAlbums = Dictionary<Int, [String]>()
         for album in library.collections! {
             if !myBestAlbums.contains(album.items[0].albumPersistentID) {
                 if album.items[0].releaseDate != nil {
-                    years.insert(calendar.component(.year, from: album.items[0].releaseDate!))
+                    let year = calendar.component(.year, from: album.items[0].releaseDate!)
+                    if !theOtherAlbums.keys.contains(year) {
+                        theOtherAlbums[year] = []
+                    }
+                    theOtherAlbums[year]!.append(album.items[0].albumTitle ?? "Title")
                 }
             }
         }
-        
-        yearPickerData = years.sorted(by: >)
+
+        theOtherAlbumsByYear = theOtherAlbums.sorted(by: { $0.0 > $1.0 })
     }
 
     override func viewDidLoad() {
@@ -68,6 +79,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
         yearPicker.delegate = self
         yearPicker.dataSource = self
+        
+        albumTable.delegate = self
+        albumTable.dataSource = self
 
         refreshLibrary()
     }
