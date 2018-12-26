98128f4a5b17f278b4dbef6f55c2841749fc4557
danmorrison <dan@danm.it>
2018-12-26 19:28:23 +1100

    Summarise the number of albums in the section header

--- The Best/ViewController.swift
+++ The Best/ViewController.swift
@@ -23,44 +23,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Add one row for summarising the year
-        return theOtherAlbumsByYear[section].value.count + 1
+        return theOtherAlbumsByYear[section].value.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return String(theOtherAlbumsByYear[section].key)
+        let count = theOtherAlbumsByYear[section].value.count
+        if count == 1 {
+            return String(theOtherAlbumsByYear[section].key) + "        " + String(count) + " album"
+        } else {
+            return String(theOtherAlbumsByYear[section].key) + "        " + String(count) + " albums"
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if theOtherAlbumsByYear[indexPath.section].value.count == indexPath.row {
-            // Return a cell summarising the number of albums in the year
-            let cell = tableView.dequeueReusableCell(withIdentifier: "summary") ?? UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "summary")
-            cell.textLabel?.text = String(theOtherAlbumsByYear[indexPath.section].value.count) + " albums"
-            cell.textLabel?.textAlignment = .center
-            cell.textLabel?.textColor = .gray
-            return cell
-        } else {
-            // Return a cell containing the album title, artist and artwork
-            let cell = tableView.dequeueReusableCell(withIdentifier: "album") ?? UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "album")
-            if theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items.count > 0 {
-                cell.textLabel?.text = theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items[0].albumTitle ?? "Title"
-                cell.detailTextLabel?.text = theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items[0].albumArtist ?? "Artist"
-                cell.detailTextLabel?.textColor = .gray
-                cell.imageView?.image = theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items[0].artwork?.image(at: CGSize(width: 44, height: 44))
-            }
-            return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "album") ?? UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "album")
+        if theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items.count > 0 {
+            cell.textLabel?.text = theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items[0].albumTitle ?? "Title"
+            cell.detailTextLabel?.text = theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items[0].albumArtist ?? "Artist"
+            cell.detailTextLabel?.textColor = .gray
+            cell.imageView?.image = theOtherAlbumsByYear[indexPath.section].value[indexPath.row].items[0].artwork?.image(at: CGSize(width: 44, height: 44))
         }
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        // If the selected row is not a summary
-        if theOtherAlbumsByYear[indexPath.section].value.count != indexPath.row {
-            musicPlayer.setQueue(with: theOtherAlbumsByYear[indexPath.section].value[indexPath.row])
-            musicPlayer.play()
-            if UIApplication.shared.canOpenURL(URL(string: "music://")!) {
-                UIApplication.shared.open(URL(string: "music://")!)
-            }
+        musicPlayer.setQueue(with: theOtherAlbumsByYear[indexPath.section].value[indexPath.row])
+        musicPlayer.play()
+        if UIApplication.shared.canOpenURL(URL(string: "music://")!) {
+            UIApplication.shared.open(URL(string: "music://")!)
         }
     }
     
